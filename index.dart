1d275ed52ea0ab73d91934a0448b879a2fa3e3cf
Decta <mo.a.baghyani@gmail.com>
2025-12-05 23:16:56 +0330

    0.0

---
+++ index.dart
@@ -0,0 +1,17 @@
+void main() {
+  // num amin;
+  // const name = "mohammad amin baghyani";
+  // print(name);
+
+  // if (name == "mohammad amin baghyani") {
+  //   print("yes");
+  // } else {
+  //   print("no");
+  // }
+  List<int> name = [1, 2, 3, 4, 5];
+  print(name);
+  name.add(12);
+  print(name);
+  Set<String> fruits = {"apple", "banana", "orange"};
+  print(fruits);
+}
